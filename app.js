5fcbac6b9e0c886033e92213d0ecedcbcac94ec7
Shah Redwan Ahmed <sahmed221241@bscse.uiu.ac.bd>
2025-10-07 23:41:15 +0600

    Add default route to respond with a welcome message

--- app.js
+++ app.js
@@ -9,6 +9,11 @@ app.use(express.json());
 const routes = require('./routes');
 app.use('/api', routes);
 
+// Default route
+app.get('/', (req, res) => {
+    res.send('Welcome to Wandernest Backend!');
+});
+
 // Start the server
 app.listen(port, () => {
     console.log(`Server is running on http://localhost:${port}`);
