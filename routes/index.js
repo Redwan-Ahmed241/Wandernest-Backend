c6ad6b1b50b6f17cef251e3a4e40edfc18572b1e
Shah Redwan Ahmed <sahmed221241@bscse.uiu.ac.bd>
2025-10-07 23:45:34 +0600

    Refactor route structure for public transport and guides APIs

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,14 @@ router.get('/', (req, res) => {
     res.send('Welcome to Wandernest API!');
 });
 
+// Public Transport API
+router.get('/public_transport/options', (req, res) => {
+    res.json({ message: 'Transport options retrieved successfully' });
+});
+
+// Guides API
+router.get('/guides', (req, res) => {
+    res.json({ message: 'Guides retrieved successfully' });
+});
+
 module.exports = router;
